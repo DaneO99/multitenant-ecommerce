849aefbc80a81839db20fef9fbda007fb99d4048
DaneO99 <dane.fitzgerald00@gmail.com>
2025-07-07 19:22:47 -0500

    Payload Integration

---
+++ src/collections/Categories.ts
@@ -0,0 +1,16 @@
+// src/collections/Categories.ts
+
+import type { CollectionConfig } from 'payload'
+
+const Categories: CollectionConfig = {
+  slug: 'categories',
+  fields: [
+    {
+      name: 'name',
+      type: 'text',
+      required: true,
+    },
+  ],
+}
+
+export default Categories
